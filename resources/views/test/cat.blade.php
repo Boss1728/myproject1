97ad3d6f0635598f75576b792158e7ab9b66fd22
Sitthi <boss_830@outlook.co.th>
2020-11-29 11:44:46 +0700

    29-11-2563

---
+++ resources/views/test/cat.blade.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+    <h1>Cat of Sitthi 61222420005</h1>
+
+<div> <img src="{{$cat}}" alt="cat" width="200" height="200">  
+
+
+
+</div>
+
+</body>
+</html>
---
+++ resources/views/test/index.blade.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+    <h1>Gallery of Sitthi 61222420005</h1>
+
+<div>
+<a href="gallery/ant">Ant <img src="{{$ant}}" alt="ant" width="200" height="200">  </a>
+<a href="gallery/bird">Bird <img src="{{$bird}}" alt="bird" width="200" height="200"> </a>
+<a href="gallery/cat">cat <img src="{{$cat}}" alt="cat" width="200" height="200"> </a>
+
+
+
+</div>
+
+</body>
+</html>
+
+
+<!-- w("test/index", compact("ant","bird","cat","god","spider") ); -->
