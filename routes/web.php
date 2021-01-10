dad9102614476b0e948080fd5ae22e9122199b3d
Boss1728 <boss_830@outlook.co.th>
2021-01-10 13:12:39 +0700

    1-10-64

--- routes/web.php
+++ routes/web.php
@@ -72,5 +72,14 @@ Route::get("/myprofile/{id}", "MyProfileController@show");
 Route::get( "/newgallery" , "MyProfileController@gallery" );
 Route::get( "/newgallery/ant" , "MyProfileController@ant" );
 Route::get( "/newgallery/bird" , "MyProfileController@bird" );
+Route::get( "/coronavirus" , "MyProfileController@coronavirus" );
+
+Route::get("/teacher" , function (){
+	return view("teacher/index");
+});
+
+Route::get("/student" , function (){
+	return view("student/index");
+});
 
 
